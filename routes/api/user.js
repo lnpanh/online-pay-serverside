1955529e966e192551c2de64be96e485dea229d6
Lâm Ngọc Phương Anh <lnpanh2109@gmail.com>
2022-03-16 15:22:26 +0700

    test cookie 2

--- routes/api/user.js
+++ routes/api/user.js
@@ -89,7 +89,7 @@ router.post('/signin', async(req, res) => {
       const accessToken = jwt.sign({userId: cur_user._id}, process.env.ACCESS_TOKEN, {
         expiresIn: process.env.jwtExpireTime * 1000})
       
-      res.cookie("accessToken", accessToken, { maxAge: process.env.jwtExpireTime * 1000, withCredentials: true, httpOnly: true, secure: true })
+      res.cookie("accessToken", accessToken, { maxAge: process.env.jwtExpireTime * 1000, withCredentials: true, httpOnly: true})
       res.status(200).json({success: true, message: "LogIn Successfully"}).end()
       }
       else 
