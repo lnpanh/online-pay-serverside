a1ceb3609a6ac6a1a934d646be1b82ab2492df41
Ngocien <51015637+Ngocien@users.noreply.github.com>
2022-04-12 16:26:56 +0700

    test15

--- routes/api/user.js
+++ routes/api/user.js
@@ -447,10 +447,10 @@ router.post('/transaction', async(req, res) => {
             if(rcv_user["hist_id"]) {
               await ListTrans.findOneAndUpdate({ _id : mongoose.Types.ObjectId(rcv_user["hist_id"])}, {$push : {TransList: newTrans_rcv}}, {session})
             } else {
-              let newList = (await ListTrans.create([{TransList: [newTrans_rcv]}], {session: session}))[0]
-              newList.newProp = true
-              newList.$session(session)
-              await newList.save()
+              const newList =  ListTrans.create([{TransList: [newTrans_rcv]}], {session: session})
+              // newList.newProp = true
+              // newList.$session(session)
+              // await newList.save()
               console.log(newList._id)
               await User.findOneAndUpdate({_id: mongoose.Types.ObjectId(rcv_user._id)}, {$set: {hist_id: newList._id}}, {session: session})
             }
