b5883f44733e4df99d8ba6aa1d8e61c82f14745c
Ngocien <51015637+Ngocien@users.noreply.github.com>
2022-04-12 15:38:59 +0700

    tess10

--- routes/api/user.js
+++ routes/api/user.js
@@ -425,6 +425,7 @@ router.post('/transaction', async(req, res) => {
   
   if (req.body.type == "transfer") {
     const rcv_user = await User.findOne({phone: req.body.phone})
+    console.log(rcv_user)
     if (!rcv_user || cur_user._id.equals(rcv_user._id)) {
       return res.status(401).json({success: false, message: "Unauthorized phone number"})
     } else {
