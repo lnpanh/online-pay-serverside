b5527306b760241c2e1f6a50a726c117e15db759
Ngocien <51015637+Ngocien@users.noreply.github.com>
2022-04-12 10:16:25 +0700

    test1

--- routes/api/user.js
+++ routes/api/user.js
@@ -176,18 +176,18 @@ router.post('/linkAcc', async(req, res) => {
     return res.status(401).json({success: false, message: "Token is expired"}).end()
   }
 
-  const cur_user = await User.findOne({_id: mongoose.Types.ObjectId(userID)})
+  const cur_user = await User.findOne({_id: mongoose.Types.ObjectId(userID)}).session(session)
   const session = await mongoose.startSession();
   session.startTransaction();
 
   // const cur_user = await User.findOne({_id: mongoose.Types.ObjectId(userID)}, {session})
-  
+  console.log("User" , cur_user)
   
   try {
     if (cur_user["acc_id"]) 
     { 
       const cur_linkAcc = await ListAcc.find({ _id : mongoose.Types.ObjectId(cur_user["acc_id"]), linkAcc: {$elemMatch: {accNum : req.body.accNum, partiesName: req.body.partiesName}}}, {session})
-      console.log("User" , cur_linkAcc)
+      
       if (cur_linkAcc.length != 0) 
       {
         await session.abortTransaction()
