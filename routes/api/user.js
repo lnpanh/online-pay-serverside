e3a432a35c68474ca4c20c73d2f6b28a912ccbd6
Ngocien <51015637+Ngocien@users.noreply.github.com>
2022-04-13 12:00:06 +0700

    add option params

--- routes/api/user.js
+++ routes/api/user.js
@@ -454,7 +454,7 @@ router.post('/transaction', async(req, res) => {
             else if (!rcv_user["hist_id"]){
               const newList =  await ListTrans.create([{TransList: [newTrans_rcv]}], {session: session})
               console.log("rcv" + newList)
-              await User.findOneAndUpdate({_id: mongoose.Types.ObjectId(rcv_user._id)}, {$set: {hist_id: newList._id} },{new: true, session: session})
+              await User.findOneAndUpdate({_id: mongoose.Types.ObjectId(rcv_user._id)}, {$set: {hist_id: newList._id} },{ returnDocument: 'after'} , {session: session})
             }
           } else {
             await session.abortTransaction()
